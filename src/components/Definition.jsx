aeb548ad8d34c39f4cbcc21e1b5a21a418ec7c1d
Adrià <adria.lop.mar@gmail.com>
2023-04-03 14:14:33 +0200

    added definition feature

---
+++ src/components/Definition.jsx
@@ -0,0 +1,23 @@
+export function Definition() {
+  return (
+    <>
+      <p className=" ml-5 flex text-3xl">
+        Not
+        <span className="  w-full relative">
+          <span className=" ml-2 phrase-animation absolute">
+            Shakespeare but I write.
+          </span>
+          <span className=" ml-2 phrase-animation ani-descartes absolute opacity-0">
+            Descartes but I think.
+          </span>
+          <span className=" ml-2 phrase-animation ani-dali absolute opacity-0">
+            Dalí but I dream.
+          </span>
+          <span className=" ml-2 phrase-animation ani-adri absolute opacity-0">
+            another, I’m Adri.
+          </span>
+        </span>
+      </p>
+    </>
+  );
+}
