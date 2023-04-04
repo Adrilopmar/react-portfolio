cc8a0b36a35e2cd5da1cee8a1559612aa59fdf6d
Adrià <adria.lop.mar@gmail.com>
2023-04-04 09:39:11 +0200

    updated tools

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -4,12 +4,12 @@ export function Tools() {
     <>
       <div className="mx-5">
         <h2 className="text-5xl ">Tools</h2>
-        <h3 className="text-2xl my-9">Technologies I use to bring your products to life:</h3>
+        <h3 className="text-2xl my-9 text-black">Technologies I use to bring your products to life:</h3>
         <div className="flex flex-wrap gap-5 justify-around mt-12">
           {tools.map((el,index)=> 
             <div key={index} className="w-2/5 my-5">
               <img  className="logo-tech-img mx-auto " src={el.image} alt={el.name}/>
-              <p className="text-center mt-3 text-xl">{el.name}</p>
+              <p className="text-center mt-3 text-xl text-black">{el.name}</p>
             </div>
             )}
         </div>
