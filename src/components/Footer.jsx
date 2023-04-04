d9c1555d73515d8ddcff55b6f3745e8d3644679c
Adrià <adria.lop.mar@gmail.com>
2023-04-04 09:37:11 +0200

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+      <>
+        <div className="bg-black py-9">
+            <h5 className="text-center text-white ">©  Adrià López Martínez 2023</h5>
+        </div>
+      </>
+    );
+  }
