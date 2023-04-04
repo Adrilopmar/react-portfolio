adc4fa8ecc8df04f6ed5ce4f1fb6feeace1fc117
Adrià <adria.lop.mar@gmail.com>
2023-04-04 09:40:02 +0200

    styling

--- src/components/Definition.jsx
+++ src/components/Definition.jsx
@@ -1,19 +1,19 @@
 export function Definition() {
   return (
     <>
-      <p className="  flex text-3xl">
+      <p className="  flex text-3xl text-black">
         Not
         <span className="  w-full relative">
-          <span className=" ml-2 phrase-animation absolute">
+          <span className=" ml-2 phrase-animation absolute text-black">
             Shakespeare but I write.
           </span>
-          <span className=" ml-2 phrase-animation ani-descartes absolute opacity-0">
+          <span className=" ml-2 phrase-animation ani-descartes absolute opacity-0 text-black">
             Descartes but I think.
           </span>
-          <span className=" ml-2 phrase-animation ani-dali absolute opacity-0">
+          <span className=" ml-2 phrase-animation ani-dali absolute opacity-0 text-black">
             Dalí but I dream.
           </span>
-          <span className=" ml-2 phrase-animation ani-adri absolute opacity-0">
+          <span className=" ml-2 phrase-animation ani-adri absolute opacity-0 text-black">
             another, I’m Adri.
           </span>
         </span>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ export function Projects() {
           className="max-w-sm rounded  mx-auto project-card relative tools-section"
         >
           {el.technologies[0].logo? <img
-            className="absolute project-main-tech"
+            className="absolute project-main-tech "
             src={el.technologies[0].logo}
             alt="main tech"
           /> :<></>}
@@ -28,10 +28,10 @@ export function Projects() {
             </div>
           </div>
           <div className="px-6 pt-4 justify-between flex gap-5">
-            <Link to={el.website} className=" link-web text-white font-bold py-2 px-4 rounded w-2/4 text-center">
+            <Link to={el.website} className="link-single-project text-white font-bold py-2 px-4 rounded w-2/4 text-center">
               Read more
             </Link>
-            <a href={el.website} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow w-2/4 text-center">
+            <a href={el.website} className="link-web bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow w-2/4 text-center">
               Visit website
             </a >
           </div>
