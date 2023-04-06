b94fe45446ecbe0c0c49a1526589b1ccd57ff828
Adrià <adria.lop.mar@gmail.com>
2023-04-06 15:09:03 +0200

    responsiveness (footer/about/tools)

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -3,7 +3,7 @@ export function Tools() {
   return (
     <>
       <div className="lg:mx-auto lg:w-4/5 xl:w-3/4">
-        <h2 className="text-5xl ">Tools</h2>
+        <h2 className="text-5xl "><strong>Tools</strong></h2>
         <h3 className="text-2xl my-9 text-black">Some of the Technologies I use to bring your products to life:</h3>
         <div className="flex flex-wrap gap-5 justify-around mt-12">
           {tools.map((el,index)=> 
@@ -13,7 +13,6 @@ export function Tools() {
             </div>
             )}
         </div>
-        
       </div>
     </>
   );
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -10,9 +10,11 @@ export function Home() {
       <div className="mx-5 md:w-4/5 md:mx-auto home-bg">
         <section className="lg:flex lg:flex-wrap lg:mx-auto lg:w-4/5 lg:justify-between xl:w-3/4">
           <div className=" flex flex-col lg:w-2/4 ">
-            <h2 className="text-5xl"><strong>G'day, this is Adrià!</strong></h2>
+            <h2 className="text-5xl">
+              <strong>G'day, this is Adrià!</strong>
+            </h2>
             <h4 className="text-3xl lg:text-3xl text-black mt-3">
-              <strong>Full stack web developer</strong> 
+              <strong>Full stack web developer</strong>
             </h4>
             <p className=" text-xl text-black">
               from Barcelona coding complex websites
@@ -22,56 +24,63 @@ export function Home() {
             </div>
           </div>
           <div className=" mx-auto lg:w-2/4 ">
-          <div className="happy-adri my-20 mx-auto lg:my-0 lg:mx-0 lg:ml-auto"> </div>
+            <div className="happy-adri my-20 mx-auto lg:my-0 lg:mx-0 lg:ml-auto">
+              {" "}
+            </div>
           </div>
           <div className="w-full lg:hidden">
             <Definition />
           </div>
-        </section >
-        </div>
-        <div className="project-bg px-5 lg:w-full lg:mx-auto">
-          <div className="md:w-4/5 md:mx-auto">
-        <section className="lg:mx-auto lg:w-4/5 lg:justify-between xl:w-3/4 ">
-          <h2 id="projects" className="text-5xl mb-12">
-          <strong>Work</strong>
-          </h2>
-          <div className=" md:flex md:flex-wrap md:justify-between">
-          <Projects />
-          </div>
         </section>
+      </div>
+      <div className="project-bg px-5 lg:w-full lg:mx-auto">
+        <div className="md:w-4/5 md:mx-auto">
+          <section className="lg:mx-auto lg:w-4/5 lg:justify-between xl:w-3/4 ">
+            <h2 id="projects" className="text-5xl mb-12">
+              <strong>Work</strong>
+            </h2>
+            <div className=" md:flex md:flex-wrap md:justify-between">
+              <Projects />
+            </div>
+          </section>
         </div>
-        </div>
-      <section className="tools-section mx-5 lg:mx-auto md:w-4/5">
+      </div>
+      <section className="tools-section mx-5 md:mx-auto md:w-4/5">
         <Tools />
       </section>
-      <section className="mx-5">
-        <h2 id="about" className="text-5xl ">
-          About
-        </h2>
-        <p>
-          {`<Adri
-class="Hard_worker Reliable"
-id="Critical_thinking"
-styles="Active_listening: true;
-Adaptability: flex;
-Teamwork: 100%; ">
-</Adri>
-<script>
-const Adri = ["Patience", "Sincere",
-"Positive", "Empathy"];
-Adri.push(Willingness_to_learn);
-if(problem && !solution){
-Adri.find(Problem_solving)
-}else if(problem > Adri){
-Adri.concat(Understanding, Resilience)};
-</script>
-</Personal Skills>`}
-        </p>
-      </section>
-
-      <section className=" social-section">
+      <div className="px-5 lg:w-full lg:mx-auto">
+        <div className="md:w-4/5 mx-auto">
+          <section className="mx-5">
+            <h2 id="about" className="text-5xl ">
+             <strong>About</strong> 
+            </h2>
+            <p>
+              {`<PersonalSkills>
+  <Adri class="Hard_worker Reliable" id="Critical_thinking" styles="Active_listening: true; Adaptability: flex; Teamwork: 100%;">
+  </Adri>
+  <script>
+    const Adri = ["Patience", "Sincere", "Positive", "Empathy"];
+    Adri.push("Willingness_to_learn");
+    const problem = true;
+    const solution = false;
+    if (problem && !solution) {
+      Adri.find("Problem_solving");
+    } else if (problem > Adri.length) {
+      Adri.concat("Understanding", "Resilience");
+    }
+  </script>
+</PersonalSkills>`}
+            </p>
+          </section>
+        </div>
+      </div>
+      <div className="px-5 md:w-full md:mx-auto social-section">
+        <div className="md:w-4/5 mx-auto">
+      <section className="m-0">
         <Social />
       </section>
+      </div>
+      </div>
       <Footer />
     </>
   );
