1572b775048f0e128a43e5550a036468f07546e8
Adrià <adria.lop.mar@gmail.com>
2023-04-13 17:08:28 +0200

    setting up deployment

--- src/services/config.contactForm.js
+++ src/services/config.contactForm.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL = axios.create({
-  baseURL: "http://localhost:8088/api/v1",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 const sendEmail = async (email) =>{
