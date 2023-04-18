db5a72e34094ebc7e163627eb7ab8bb4bc66f898
Adrià <adria.lop.mar@gmail.com>
2023-04-18 21:07:15 +0200

    fixed margins

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -24,8 +24,8 @@ export function Home() {
   }
   return (
     <>
-      <div className="mx-5 md:w-4/5 md:mx-auto home-bg">
-        <section className="lg:flex lg:flex-wrap lg:mx-auto lg:w-4/5 lg:justify-between xl:w-3/4">
+      <div className="mx-5 md:w-4/5 md:mx-auto ">
+        <div className="welcome-section lg:flex lg:flex-wrap lg:mx-auto lg:w-4/5 lg:justify-between xl:w-3/4">
           <div className=" flex flex-col lg:w-2/4 ">
             <h2 className="text-5xl">
               <strong>G'day, this is Adrià!</strong>
@@ -48,11 +48,11 @@ export function Home() {
           <div className="w-full lg:hidden">
             <Definition />
           </div>
-        </section>
+        </div>
       </div>
-      <div className="project-bg px-5 lg:w-full lg:mx-auto">
+      <div className="project-bg  lg:w-full lg:mx-auto">
         <div className="md:w-4/5 md:mx-auto">
-          <section className="lg:mx-auto lg:w-4/5 lg:justify-between xl:w-3/4 ">
+          <section className="lg:mx-auto mx-5 lg:w-4/5 lg:justify-between xl:w-3/4 ">
             <h2 id="projects" className="text-5xl mb-12">
               <strong>Work</strong>
             </h2>
@@ -65,20 +65,20 @@ export function Home() {
       <section className="tools-section mx-5 md:mx-auto md:w-4/5">
         <Tools projectList={projectList}/>
       </section>
-      <div className="px-5 lg:w-full lg:mx-auto">
+      <div className="lg:w-full lg:mx-auto">
         <div className="md:w-4/5 mx-auto">
-          <section className="mx-5 lg:mx-auto lg:w-4/5 lg:justify-between xl:w-3/4">
+          <section className="mx-5 lg:mx-auto lg:w-4/5 lg:justify-between xl:w-3/4 pb-0 mb-0">
             <h2 id="about" className="text-5xl ">
               <strong>About</strong>
             </h2>
             <div className="mt-9">
               <img
-                className="hidden md:block"
+                className="hidden lg:block"
                 src="https://res.cloudinary.com/djqzi4hgo/image/upload/v1681217778/React-portfolio/personal%20skills/pers-skills_rl3taf.png"
                 alt=""
               />
               <img
-                className="md:hidden block"
+                className="lg:hidden block"
                 src="https://res.cloudinary.com/djqzi4hgo/image/upload/v1681218317/React-portfolio/personal%20skills/skills-mobile_rc0bet.png"
                 alt=""
               />
@@ -86,7 +86,7 @@ export function Home() {
           </section>
         </div>
       </div>
-      <div className="px-5 lg:w-full lg:mx-auto">
+      <div className="lg:w-full lg:mx-auto">
         <div className="md:w-4/5 mx-auto lg:flex justify-between">
           <section className="mx-5 lg:mx-auto lg:w-4/5 lg:justify-between xl:w-3/4 ">
           <h2 id="about" className="text-5xl mb-12">
