0995e0c2c8e6f3e9e8785c4f78b4fcbab76627d4
Adrià <adria.lop.mar@gmail.com>
2023-04-18 10:10:20 +0200

    mail regex added

--- src/controller/config.contactForm.js
+++ src/controller/config.contactForm.js
@@ -1,9 +1,12 @@
 import { sendEmail } from "../services/config.contactForm"
 
+const regexMail = new RegExp(`([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|\[[\t -Z^-~]*])`)
+
 const handleSendEmail =(email)=>{
     return sendEmail(email)
 }
 
 export{
-    handleSendEmail
+    handleSendEmail,
+    regexMail
 }
