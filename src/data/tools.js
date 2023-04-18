53bddf2370d0158f2ced263ba4c532ac884d811d
Adrià <adria.lop.mar@gmail.com>
2023-04-18 10:52:19 +0200

    added tools

---
+++ src/data/tools.js
@@ -0,0 +1,58 @@
+export const tools =[
+    {
+        "name":"HTML5",
+        "image": "https://res.cloudinary.com/djqzi4hgo/image/upload/v1667916398/vue%20portfolio/technologies/html5_k9flxd.png"
+    },
+    {
+        "name":"CSS3",
+        "image": "https://res.cloudinary.com/djqzi4hgo/image/upload/v1667916398/vue%20portfolio/technologies/css_xcjpia.png"
+    },
+    {
+        "name":"JavaScript",
+        "image": "https://res.cloudinary.com/djqzi4hgo/image/upload/v1667916397/vue%20portfolio/technologies/JavaScript_yrhnxz.png"
+    },
+    {
+        "name":"TypeScript",
+        "image": "https://res.cloudinary.com/djqzi4hgo/image/upload/v1668095297/vue%20portfolio/technologies/typescript_okxmwe.svg"
+    },
+    {
+        "name":"React",
+        "image": "https://res.cloudinary.com/djqzi4hgo/image/upload/v1680526933/React-portfolio/Technologies/React-icon.svg_dkz23l.png"
+    },
+    {
+        "name":"Vue",
+        "image":  "https://res.cloudinary.com/djqzi4hgo/image/upload/v1667916397/vue%20portfolio/technologies/vue_mzxjre.png"
+    },
+    {
+        "name":"Angular",
+        "image":  "https://res.cloudinary.com/djqzi4hgo/image/upload/v1668095157/vue%20portfolio/technologies/angular_vqr5ek.png"
+    },
+    {
+        "name":"Vite",
+        "image":   "https://res.cloudinary.com/djqzi4hgo/image/upload/v1667916397/vue%20portfolio/technologies/vite_r8wnjf.png"
+    },
+    {
+        "name":"Node js",
+        "image":   "https://res.cloudinary.com/djqzi4hgo/image/upload/v1680527820/React-portfolio/Technologies/590px-Node.js_logo.svg_ushzuh.png"
+    },
+    {
+        "name":"Java",
+        "image":   "https://res.cloudinary.com/djqzi4hgo/image/upload/v1680527552/React-portfolio/Technologies/java_yysd4w.png"
+    },
+    {
+        "name":"MySQL",
+        "image":    "https://res.cloudinary.com/djqzi4hgo/image/upload/v1680527668/React-portfolio/Technologies/pngwing.com_yxeebf.png"
+    },
+    {
+        "name":"API Rest",
+        "image":"https://res.cloudinary.com/djqzi4hgo/image/upload/v1680527484/React-portfolio/Technologies/api-icon-e1454693853722_gm926m.png"
+    },
+    {
+        "name":"Bootstrap",
+        "image":"https://res.cloudinary.com/djqzi4hgo/image/upload/v1668095461/vue%20portfolio/technologies/bootstrap_gdxtwx.svg"
+    },
+    {
+        "name":"Tailwind",
+        "image":"https://res.cloudinary.com/djqzi4hgo/image/upload/v1667917977/vue%20portfolio/technologies/tailwind_nbomhd.png"
+    } 
+]
