a49b7fb2f77adbe15a536b53b9d28e6b44c015fb
Adrià <adria.lop.mar@gmail.com>
2023-04-18 21:09:17 +0200

    fixed warning

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -2,25 +2,25 @@ export function Success() {
   return (
     <>
     <div className=" w-full lg:w-2/4 my-14">
-    <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
-  <div class="flex">
-    <div class="py-1"></div>
+    <div className="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+  <div className="flex">
+    <div className="py-1"></div>
     <div className="flex gap-9 align-middle">
-    <div class="wrapper">
+    <div className="wrapper">
         {" "}
         <svg
-          class="checkmark"
+          className="checkmark"
           >
           {" "}
           <circle
-            class="checkmark__circle"
+            className="checkmark__circle"
             cx="26"
             cy="26"
             r="25"
             fill="none"
             />{" "}
           <path
-            class="checkmark__check"
+            className="checkmark__check"
             fill="none"
             d="M14.1 27.2l7.1 7.2 16.7-16.8"
             />
