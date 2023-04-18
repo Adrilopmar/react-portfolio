ba45a27df4a15120a145857792489f1e701c0260
Adrià <adria.lop.mar@gmail.com>
2023-04-18 21:07:57 +0200

    navbar state lifted up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import Navbar from "./components/NavbarMenu";
 import { Home } from "./views/Home";
+import { useState } from "react";
 
 function App() {
+const [isMenuActive, setIsActive] = useState(false);
+
+const handleActive = () => setIsActive(!isMenuActive);
+
   const scroll =(to)=>{
     document.getElementById(to).scrollIntoView({ behavior: 'smooth', block: 'start' })
+    setIsActive(!isMenuActive);
   }
   return (
     <>
-      <Navbar scroll={scroll}/>
+      <Navbar scroll={scroll} handleActive={handleActive} isMenuActive={isMenuActive}/>
       <Home/>
       <Routes>
         {/* <Route></Route> */}
